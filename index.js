c4a758c076bbfd7c5c8f8e3a82d5bd6a65ec8f9d
Vladislav Smirnov <mr.bikus@yandex.ru>
2021-08-06 00:28:32 +0200

    🚧 in progress

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 //https://service.berlin.de/terminvereinbarung/termin/tag.php?dienstleister=122251&herkunft=http%3A%2F%2Fservice.berlin.de%2Fstandort%2F122251%2F&termin=1&anliegen%5B%5D=120686
 
 const express = require('express');
-const axios = require('axios');
+//const axios = require('axios');
 
 const app = new express();
 
-const TOKEN = process.env.TOKEN;
+//const TOKEN = process.env.TOKEN;
 
 //axios.get(`https://api.telegram.org/bot${TOKEN}/setWebhook?url=https://berlin-termin-buchen-bot.herokuapp.com/bot`);
 
 app.get('/', (req, res) => {
-    res.send(TOKEN);
+    res.send('TOKEN');
 });
 /*
 
@@ -28,6 +28,6 @@ app.post('/bot', (req, res) => {
 });
 */
 
-app.listen(443, function() {
+app.listen(8080, function() {
     console.log("Telegram app listening on port 3000!")
 });
