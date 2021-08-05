d6555f2c0734b741808b0a30c06b2449217dbd41
Vladislav Smirnov <mr.bikus@yandex.ru>
2021-08-06 01:15:59 +0200

    🚧 in progress

--- index.js
+++ index.js
@@ -32,7 +32,11 @@ app.post('/bot', (req, res) => {
                 chat_id: message.chat.id,
                 text: message,
             }
-        );
+        ).then((response) => {
+            // We get here if the message was successfully posted
+            console.log("Message posted")
+            res.end("ok")
+        });
 });
 
 app.listen(process.env.PORT || 3000, function() {
