5cd5a7e6859d76a710f85097275c18549023dacd
Vladislav Smirnov <mr.bikus@yandex.ru>
2021-08-07 12:52:41 +0200

    🚧 in progress

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
 ); // for parsing application/x-www-form-urlencoded
 
 
-axios.get(`https://api.telegram.org/bot${TOKEN}/setWebhook?url=https://berlin-termin-buchen-bot.herokuapp.com/bot`);
+axios.get(`https://api.telegram.org/bot${TOKEN}/setWebhook?url=https://berlin-termin-buchen.netlify.app/bot`);
 
 app.get('/', (req, res) => {
     res.send(TOKEN);
