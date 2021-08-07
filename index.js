05959e11e3eeaf4bbb766e031bf18407d0e6d54a
Vladislav Smirnov <mr.bikus@yandex.ru>
2021-08-07 02:44:45 +0200

    🚧 in progress

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
 ); // for parsing application/x-www-form-urlencoded
 
 
-axios.get(`https://api.telegram.org/bot${TOKEN}/setWebhook?url=https://berlin-termin-buchen-bot.herokuapp.com/bot`);
+//axios.get(`https://api.telegram.org/bot${TOKEN}/setWebhook?url=https://berlin-termin-buchen-bot.herokuapp.com/bot`);
 
 app.get('/', (req, res) => {
     res.send(TOKEN);
@@ -38,6 +38,48 @@ app.post('/bot', (req, res) => {
             res.end("ok")
         });
 });
+
+/*setInterval(() => {
+}, 60000);*/
+    axios({
+        url: 'https://service.berlin.de/terminvereinbarung/termin/tag.php?dienstleister=122251&herkunft=http%3A%2F%2Fservice.berlin.de%2Fstandort%2F122251%2F&termin=1&anliegen%5B%5D=120686',
+        method: 'get',
+        maxRedirects: 0,
+    })
+        .catch(response => {
+            console.log(response.response.status);
+            if (response.response.status === 302) {
+                return axios({
+                    url: 'https://service.berlin.de/terminvereinbarung/termin/day/',
+                    method: 'get',
+                    headers: {
+                        cookie: response.response.headers['set-cookie'][0].split(';')[0]
+                    }
+                });
+            }
+            return null;
+        })
+        .then(response => {
+            if (response != null) {
+                const regex = /[^t]\s?(buchbar)/gm;
+                let res = [...response.data.matchAll(regex)];
+                if (res.length > 1) {
+                    console.log('FOUND');
+                } else {
+                    axios
+                        .post(
+                            `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+                            {
+                                text: 'No free dates 😭',
+                            }
+                        ).then((response) => {
+                            // We get here if the message was successfully posted
+                            console.log("Message posted")
+                            res.end("ok")
+                        });
+                }
+            }
+        });
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("Telegram app listening on port 3000!")
